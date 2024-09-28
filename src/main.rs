74c734d0465c3e683d4245e9eb4227d4e00c7b05
0xpapercut <0xpapercut@gmail.com>
2024-09-28 23:37:14 +0200

    Setup cli

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,12 @@
-// use anyhow::Error;
-// use solana_client::rpc_config::RpcBlockConfig;
-// use solana_transaction_status::UiConfirmedBlock;
-// use tokio;
-// use anyhow::anyhow;
-// use std::sync::Arc;
-// use std::pin::Pin;
-// use std::future::Future;
-// use std::sync::Mutex;
-// use futures_util::StreamExt;
-// use serde::{Serialize, Deserialize};
-// use solana_client::rpc_response::RpcBlockUpdate;
-// use std::time::SystemTime;
-// use chrono::{DateTime, Utc};
-// use clickhouse::serde::time::datetime;
-// // use time::PrimitiveDateTime;
-// use tokio::signal;
-// use tokio::sync::watch;
-// use tokio::sync::mpsc;
-// use tokio::sync::Notify;
-// use time::OffsetDateTime;
-// use tokio_util::sync::CancellationToken;
-
-// use solana_client::nonblocking::{pubsub_client::PubsubClient, rpc_client::RpcClient};
-// use solana_client::rpc_config::{RpcBlockSubscribeConfig, RpcBlockSubscribeFilter};
-// use solana_sdk::commitment_config::CommitmentConfig;
-// use solana_transaction_status::{EncodedConfirmedBlock, UiTransactionEncoding};
-
-// use clickhouse::{Client as ClickhouseClient, Row};
-// // use indexer
-
-// // use crate::in
-
-// #[tokio::main]
-// async fn main() {
-//     let clickhouse_user = std::env::var("CLICKHOUSE_USER").expect("CLICKHOUSE_USER environment variable not set");
-//     let clickhouse_password = std::env::var("CLICKHOUSE_PASSWORD").expect("CLICKHOUSE_PASSWORD environment variable not set");
-//     let clickhouse_client = ClickhouseClient::default()
-//         .with_url("http://localhost:8123")
-//         .with_user(clickhouse_user)
-//         .with_password(clickhouse_password)
-//         .with_database("solana_lite_indexer");
-
-//     let websocket_url = std::env::var("SOLANA_WEBSOCKET").expect("SOLANA_WEBSOCKET environment variable not set");
-//     let rpc_url = std::env::var("SOLANA_HTTP").expect("SOLANA_HTTP environment variable not set");
-//     let pubsub_client = PubsubClient::new(&websocket_url).await.unwrap();
-//     let rpc_client = RpcClient::new(rpc_url);
-
-//     let indexer = Box::leak(Box::new(Indexer::new(rpc_client, pubsub_client, clickhouse_client)));
-//     let shutdown_tx = watch::Sender::new(());
-
-//     let shutdown_rx = shutdown_tx.subscribe();
-//     let start = tokio::spawn(async move {
-//         indexer.start(shutdown_rx).await.unwrap();
-//     });
-
-//     let mut shutdown_rx = shutdown_tx.subscribe();
-//     tokio::select! {
-//         _ = signal::ctrl_c() => { shutdown_tx.send(()).unwrap(); },
-//         _ = shutdown_rx.changed() => {},
-//     }
-//     println!("Shutting down...");
-//     start.await.unwrap();
-// }
-
+use std::fs;
+use clap::{Parser, Subcommand};
 use tokio;
 use tokio::signal;
 use tokio::sync::watch;
+use serde::{Serialize, Deserialize};
 
 use solana_client::nonblocking::{pubsub_client::PubsubClient, rpc_client::RpcClient};
-use clickhouse::Client as ClickhouseClient;
+use clickhouse::{Row, Client as ClickhouseClient};
 
 pub mod indexer;
 use indexer::Indexer;
@@ -77,28 +15,36 @@ pub mod table;
 pub mod transaction;
 pub mod instruction;
 
+#[derive(Parser)]
+#[command(name = "solana_lite_indexer")]
+struct Cli {
+    #[arg(long)]
+    setup_db: bool,
+    // #[command(subcommand)]
+    // command: Option<Commands>,
+}
 
-// use crate::indexer;
-
-// mod instruction;
+#[derive(Subcommand)]
+enum Commands {
+    #[command(name = "setup-db")]
+    SetupDb,
+}
 
 #[tokio::main]
 async fn main() {
-    let clickhouse_user = std::env::var("CLICKHOUSE_USER").expect("CLICKHOUSE_USER environment variable not set");
-    let clickhouse_password = std::env::var("CLICKHOUSE_PASSWORD").expect("CLICKHOUSE_PASSWORD environment variable not set");
-    let clickhouse_client = ClickhouseClient::default()
-        .with_url("http://localhost:8123")
-        .with_user(clickhouse_user)
-        .with_password(clickhouse_password)
-        .with_database("solana_lite_indexer")
-        .with_option("async_insert", "1")
-        .with_option("wait_for_async_insert", "0");
+    let cli = Cli::parse();
 
-    let websocket_url = std::env::var("SOLANA_WEBSOCKET").expect("SOLANA_WEBSOCKET environment variable not set");
-    let rpc_url = std::env::var("SOLANA_HTTP").expect("SOLANA_HTTP environment variable not set");
-    let pubsub_client = PubsubClient::new(&websocket_url).await.unwrap();
-    let rpc_client = RpcClient::new(rpc_url);
+    if cli.setup_db {
+        let clickhouse_client = get_clickhouse_client();
+        setup_db(clickhouse_client).await;
+    } else {
+        let clickhouse_client = get_clickhouse_client();
+        let (rpc_client, pubsub_client) = get_rpc_and_pubsub().await;
+        run_indexer(clickhouse_client, rpc_client, pubsub_client).await
+    }
+}
 
+async fn run_indexer(clickhouse_client: ClickhouseClient, rpc_client: RpcClient, pubsub_client: PubsubClient) {
     let indexer = Box::leak(Box::new(Indexer::new(rpc_client, pubsub_client, clickhouse_client)));
     let shutdown_tx = watch::Sender::new(());
 
@@ -114,4 +60,46 @@ async fn main() {
     }
     println!("Shutting down...");
     start.await.unwrap();
+}
+
+#[derive(Serialize, Deserialize, Debug, Row)]
+pub struct TablesRow {
+    name: String,
+}
+
+async fn setup_db(clickhouse_client: ClickhouseClient) {
+    let tables: Vec<TablesRow> = clickhouse_client.query("SHOW TABLES").fetch_all().await.unwrap();
+    for table in tables {
+        clickhouse_client.query(&format!("DROP TABLE {}", table.name)).execute().await.unwrap();
+    }
+    println!("Database has been cleaned");
+
+    let schema = fs::read_to_string("./schema.sql").unwrap();
+    for statement in schema.split(';') {
+        if !statement.trim().is_empty() {
+            clickhouse_client.query(statement).execute().await.unwrap();
+        }
+    }
+    println!("Tables sucessfully inserted");
+}
+
+async fn get_rpc_and_pubsub() -> (RpcClient, PubsubClient) {
+    let rpc_url = std::env::var("SOLANA_HTTP").expect("SOLANA_HTTP environment variable not set");
+    let rpc_client = RpcClient::new(rpc_url);
+    let websocket_url = std::env::var("SOLANA_WEBSOCKET").expect("SOLANA_WEBSOCKET environment variable not set");
+    let pubsub_client = PubsubClient::new(&websocket_url).await.unwrap();
+    (rpc_client, pubsub_client)
+}
+
+fn get_clickhouse_client() -> ClickhouseClient {
+    let clickhouse_user = std::env::var("CLICKHOUSE_USER").expect("CLICKHOUSE_USER environment variable not set");
+    let clickhouse_password = std::env::var("CLICKHOUSE_PASSWORD").expect("CLICKHOUSE_PASSWORD environment variable not set");
+    let clickhouse_client = ClickhouseClient::default()
+        .with_url("http://localhost:8123")
+        .with_user(clickhouse_user)
+        .with_password(clickhouse_password)
+        .with_database("solana_lite_indexer")
+        .with_option("async_insert", "1")
+        .with_option("wait_for_async_insert", "0");
+    clickhouse_client
 }
